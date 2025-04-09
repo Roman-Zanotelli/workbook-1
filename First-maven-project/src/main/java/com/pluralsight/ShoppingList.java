ebf69c1187cb61ea14b5723785d90d7f42edbc66
Roman-Zanotelli <Roman-Zanotelli@github.com>
2025-04-09 12:33:20 -0400

    added shopping list

---
+++ First-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Apple\nBanana\nOrange\nCheese\nMilk\nChips\nBread\nWater\nSoda\nPaper-Towels");
+    }
+}
