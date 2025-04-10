52f66737a2dce6da270e521f902b154f70168fad
Roman-Zanotelli <Roman-Zanotelli@github.com>
2025-04-10 10:04:57 -0400

    Completed Mod3 - Exercise 3 - Basic Calculator

---
+++ BasicCalculator/src/main/java/com/pluralsight/BasicCalculator.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        float first = scanner.nextFloat();
+        System.out.print("Enter second number: ");
+        float second = scanner.nextFloat();
+        System.out.print("Possible calculations:\n(A)dd\n(S)ubtract \n(M)ultiply \n(D)ivide\nPlease select an option: ");
+        String option = scanner.next();
+        System.out.println(first + " * " + second + " = " + first * second);
+    }
+}
