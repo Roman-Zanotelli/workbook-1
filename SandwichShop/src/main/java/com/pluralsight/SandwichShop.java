0f0a3ff4ec7332a34b53b7e9e4b8797f3fc54a8e
Roman-Zanotelli <Roman-Zanotelli@github.com>
2025-04-11 12:15:14 -0400

    Added clarifying Comments

--- SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
+++ SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
@@ -11,7 +11,7 @@ public class SandwichShop {
             return;
         }
         int selection = scanner.nextInt();
-        float price = selection == 1 ? 5.45f : selection == 2 ? 8.95f : 0;
+        float price = selection == 1 ? 5.45f : selection == 2 ? 8.95f : 0; //calculates the price based of previous selection defaulting to 0 for invalid selections
         if (price == 0) { //check for invalid selection
             System.out.println("Invalid Selection");
             return;
@@ -19,7 +19,7 @@ public class SandwichShop {
         System.out.print("Would you like to make your sandwich loaded?\n- Regular: +$1.00\n- Large: +$1.75\n Select (Y/N): ");
         String is_loaded = scanner.next();
         if (is_loaded.equalsIgnoreCase("Y") || is_loaded.equalsIgnoreCase("Yes")){ //check if the response was yes
-            price += selection == 1 ? 1 : 1.75f;
+            price += selection == 1 ? 1 : 1.75f; //checks which sandwich selection was previously made and adds the appropriate price increase
         } else if (!(is_loaded.equalsIgnoreCase("N") || is_loaded.equalsIgnoreCase("No"))){ //check if there was an invalid input
             System.out.println("Invalid Input");
             return;
@@ -30,8 +30,7 @@ public class SandwichShop {
             return;
         }
         int age = scanner.nextInt();
-
-        float discount = age <= 17 ? .9f : age >= 65 ? .8f : 1;
+        float discount = age <= 17 ? .9f : age >= 65 ? .8f : 1; //calculates the discount based off age range
         System.out.println("Total Price After Discount: " + discount * price);
 
     }
